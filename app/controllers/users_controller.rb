43e7de3134a67b266c268626a7fb1d96f5d59030
Dan Haugen <dbhaug11@smumn.edu>
2013-04-11 18:17:22 -0500

    added edit profile page user story 14

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,4 +26,19 @@ class UsersController < ApplicationController
   def index
 
   end
+
+  def edit
+    @user=User.find_by_id(params[:id])
+  end
+
+  def update
+    @user=User.find_by_id(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 end
