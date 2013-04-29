81b7b7f0d163ccc3300e8ce53d2400635b4c500c
Dan Haugen <dbhaug11@smumn.edu>
2013-04-28 19:26:48 -0500

    started testing User Story: 2,3,6

--- spec/requests/user_requests.rb
+++ spec/requests/user_requests.rb
@@ -8,6 +8,7 @@ describe "User" do
        	password_confirmation: 'daniel5' )}
 
 	let (:item) {Item.create(name: Movie)}
+	let (:review {Review.create(item_id: :item.id, review: "This is a review")})
 
 	before(:each) do
 		sign_in user
@@ -17,13 +18,29 @@ describe "User" do
 	describe "home" do
 		it {should have_selector('div.item')}
 		it {should have_selector('div.star')}
+		
+		describe "viewing items" do
+			click_link("Movie")
+			it {should have_selector('h1',text: "Movie")}
+			it {should have_selector('h5',text: review.review)}
+		end
+
+		describe "browse items" do
+			click_link("Browse")
+			it {should have_selector('h1', text: "Items")}
 	end
 
 	describe "reviews" do
 		it {should have_link('Review', href: new_item_review_path)}
-		click_link('delete', href: new_item_review_path)
+		click_link('Review', href: new_item_review_path)
 		fill_in("#new_review", "good")
 		click_button('submit')
 		it {should have_selector('h5',text: 'good')}
+
+		describe "flag" do
+			it {should have_link("Flag", href: review_path(review))}
+			click_link("Flag", href: review_path(review))
+			it {should_not have_link("Flag", href: review_path)}
+		end
 	end
 end
